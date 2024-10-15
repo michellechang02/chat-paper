42f56a3af2396ab086969c30f9f9e720da6585a9
michellechang02 <changmi@seas.upenn.edu>
2024-10-15 12:21:50 -0400

    fix api

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", 
+                   "https://michellechang02.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
